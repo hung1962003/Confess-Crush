12293eebaf9e6dc10bc00dffde97d7d55245e317
hung1962003 <106923246+hung1962003@users.noreply.github.com>
2022-06-05 23:41:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Bae à",
-    introTitle: 'Phương Vy ',
+    titleWeb: "thế giới của bé:>>",
+    introTitle: 'Bae à ',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao chị cứ đi nhầm đường
+    Lạc vào tim bé lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Yêu ơi là yêu <33',
-    btnNo: 'Cậu mơ ah :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    title: 'Phải chăng chị đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng chị đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Yêu lắm luôn ah <33',
+    btnNo: 'Bé nằm mơ sao :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao chị lại yêu mình bé thế <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: '19 ngày mình đi ',
+    reply: 'Yêu thì yêu mà không yêu thì vẫn yêu <33333333',
+    mess: 'Bé biết mà 🥰. Hổng có chi nha Hổng có chi nha 🥰',
+    messDesc: '19 ngày nữa chị rảnh ko ngày mình đi ',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/messages/t/100028912640019' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
